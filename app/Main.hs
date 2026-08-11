637789ceb696afe186b7b75928cea9caaa38e050
VivekVenigalla <46229791+VivekVenigalla@users.noreply.github.com>
2026-08-11 10:18:14 -0400

    fixed bugs and warnings with tabs and spaces

--- app/Main.hs
+++ app/Main.hs
@@ -6,5 +6,16 @@ import Math.Vector3 --import Vector3 functions and types
 
 main :: IO ()
 main = do
-	putStrLn "Hello, Haskell!"
-	show Vec3 1 2 3
+    putStrLn "Hello, Haskell!"
+    --let creates variables without side effects
+    let v1 = vec3 1.0 2.0 3.0
+        v2 = vec3 4.0 (-5.0) 6.0
+        v3 = v1 ^+^ v2
+        dot = v1 <.> v2
+        cross = v1 >< v2
+
+    putStrLn $ "v1: " ++ show v1
+    putStrLn $ "v2: " ++ show v2
+    putStrLn $ "v1 + v2: " ++ show v3
+    putStrLn $ "v1 . v2: " ++ show dot
+    putStrLn $ "v1 x v2: " ++ show cross
--- src/Math/Vector3.hs
+++ src/Math/Vector3.hs
@@ -1,8 +1,7 @@
-{#-OPTIONS -WALL #-} --turns on warnings for any type conflicts
 
 module Math.Vector3 where --define the file name and location so the .cabal file can find the module
 
-type R :: Double --convert the Double into a new alias for real numbers
+type R = Double --convert the Double into a new alias for real numbers
 
 --create custom vector operators with their corresponding precedence
 -- ^ indicates that that side is a vector
@@ -19,58 +18,59 @@ infixl 7 >< --cross product
 
 --create function showDouble
 showDouble :: R->String --takes a real number and returns a string. if it is a negative number enclose in parantheses
-showDouble x =
-	| x < 0 = "(" ++ show x ++ ")"
-	| otherwise = show x
+showDouble x = if x <0 then "(" ++ show x ++ ")" else show x
 
 --create Vector3 Data object
 
-data Vector3 = Vector3{ xComp :: R, --components
-						yComp :: R,
-						zComp :: R
-					} deriving(Eq) --functions deriving for this data type(equals function)
+data Vector3 = Vector3
+ {xComp :: {-# UNPACK #-} !R, --components
+  yComp :: {-# UNPACK #-} !R, --unpack and ! enforce strictness on the data so they are not transferred to the heap
+  zComp :: {-# UNPACK #-} !R
+ }deriving(Eq) --functions deriving for this data type(equals function)
 
 --create a instance of the show class to allow for Vector3 printing
 
 instance Show Vector3 where
-	show (Vector3 x y z) = "vec" ++ showDouble x ++ " " ++ showDouble y ++ " " showDouble z
+ show (Vector3 x y z) = "vec" ++ showDouble x ++ " " ++ showDouble y ++ " " ++ showDouble z
 
 --create function that creates the data object
-Vec3:: R -> R -> R -> Vector3 --R are all of the components
+--NOTE HELPER FUNCTIONS SHOULD BE LOWERCASE
+vec3 :: R -> R -> R -> Vector3 --R are all of the components
 
-Vec3 = Vector3 --using point free notation to fill in the inputs
+vec3 = Vector3 --using point free notation to fill in the inputs
+
 
 
 --create Vector functions
 
 --create all the hats
 iHat :: Vector3
-iHat = Vec3 1 0 0
+iHat = vec3 1 0 0
 
 jHat :: Vector3
-jHat = Vec3 0 1 0
+jHat = vec3 0 1 0
 
 kHat :: Vector3
-kHat = Vec3 0 0 1
+kHat = vec3 0 0 1
 
 zeroV :: Vector3
-zeroV = Vec3 0 0 0
+zeroV = vec3 0 0 0
 
 --create basic operations
 (^+^) :: Vector3 -> Vector3 -> Vector3
-Vector3 ax ay az ^+^ Vector3 bx by bz = Vector3	(ax+bx) (ay+by) (az+bz)
+Vector3 ax ay az ^+^ Vector3 bx by bz = Vector3 (ax+bx) (ay+by) (az+bz)
 
 (^-^) :: Vector3 -> Vector3 -> Vector3
-Vector3 ax ay az ^-^ Vector3 bx by bz = Vector3	(ax-bx) (ay-by) (az-bz)
+Vector3 ax ay az ^-^ Vector3 bx by bz = Vector3 (ax-bx) (ay-by) (az-bz)
 
 (*^) :: R -> Vector3 -> Vector3
-x *^ Vector3 bx by bz = Vector3	(x*bx) (x*by) (x*bz)
+x *^ Vector3 bx by bz = Vector3 (x*bx) (x*by) (x*bz)
 
 (^*) :: Vector3 -> R -> Vector3
-Vector3 bx by bz ^* x = Vector3	(x*bx) (x*by) (x*bz)
+Vector3 bx by bz ^* x = Vector3 (x*bx) (x*by) (x*bz)
 
 (^/) :: Vector3 -> R -> Vector3
-Vector3 bx by bz ^/ x = Vector3	(bx/x) (by/x) (bz/x)
+Vector3 bx by bz ^/ x = Vector3 (bx/x) (by/x) (bz/x)
 
 --dot product
 (<.>) :: Vector3 -> Vector3 -> R 
@@ -78,7 +78,7 @@ Vector3 ax ay az <.> Vector3 bx by bz = (ax*bx) + (ay*by) + (az*bz)
 
 --cross product(use determinant method)
 (><) :: Vector3 -> Vector3 -> Vector3
-Vector3 ax ay az <.> Vector3 bx by bz = Vector3 (ay*bz - az*by) (ax*bz - az*bx) (ax*by - ay*bx)
+Vector3 ax ay az >< Vector3 bx by bz = Vector3 (ay*bz - az*by) (ax*bz - az*bx) (ax*by - ay*bx)
 
 
 
